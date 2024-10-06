a3e23528019ef6a357c3adde7d60355d6263d0f6
Anish <work.anish.004@gmail.com>
2024-10-06 15:42:43 +0530

    created user model

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ import { connectDb } from "./lib/db.js"
 dotenv.config()
 const app = express()
 
+app.use(express.json())
+
 const PORT= process.env.PORT || 4000
 
 app.use('/api/v2/auth',authRoutes)
