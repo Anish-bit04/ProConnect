995cc7ee57a8e0209e165c4246f773fb4b227d5f
Anish <work.anish.004@gmail.com>
2024-10-07 03:03:32 +0530

    created api endpoint to likePost and createComment

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,4 +1,5 @@
 import cloudinary from "../lib/cloudinary";
+import Notification from "../models/notification.model";
 import Post from "../models/posts.model";
 
 export const getFeedPosts = async (req, res) => {
@@ -83,10 +84,10 @@ export const deletePost = async (req, res) => {
 export const getPostById = async (req, res) => {
     try {
         const postId = req.params.id;
-        
+
         const post = await Post.findById(postId)
-        .populate("author","name username profilePicture headline")
-        .populate("comments.user","name username profilePicture headline")
+        .populate("author","name email username profilePicture headline")
+        .populate("comments.user","name email username profilePicture headline")
 
         if (!post) {
             return res.status(404).json({ message: "Post not found" });
@@ -98,3 +99,84 @@ export const getPostById = async (req, res) => {
         res.status(500).json({ message: "Internal Server Error" });
     }
 }
+
+
+
+export const createComment = async (req, res) => {
+	try {
+		const postId = req.params.id;
+		const { content } = req.body;
+
+		const post = await Post.findByIdAndUpdate(
+			postId,
+			{
+				$push: { comments: { user: req.user._id, content } },
+			},
+			{ new: true }
+		).populate("author", "name email username headline profilePicture");
+
+		// create a notification if the comment owner is not the post owner
+		if (post.author._id.toString() !== req.user._id.toString()) {
+			const newNotification = new Notification({
+				recipient: post.author,
+				type: "comment",
+				relatedUser: req.user._id,
+				relatedPost: postId,
+			});
+
+			await newNotification.save();
+
+			try {
+				const postUrl = process.env.CLIENT_URL + "/post/" + postId;
+				await sendCommentNotificationEmail(
+					post.author.email,
+					post.author.name,
+					req.user.name,
+					postUrl,
+					content
+				);
+			} catch (error) {
+				console.log("Error in sending comment notification email:", error);
+			}
+		}
+
+		res.status(200).json(post);
+	} catch (error) {
+		console.error("Error in createComment controller:", error);
+		res.status(500).json({ message: "Server error" });
+	}
+};
+
+export const likePost = async (req, res) => {
+	try {
+		const postId = req.params.id;
+		const post = await Post.findById(postId);
+		const userId = req.user._id;
+
+		if (post.likes.includes(userId)) {
+			// unlike the post
+			post.likes = post.likes.filter((id) => id.toString() !== userId.toString());
+		} else {
+			// like the post
+			post.likes.push(userId);
+			// create a notification if the post owner is not the user who liked
+			if (post.author.toString() !== userId.toString()) {
+				const newNotification = new Notification({
+					recipient: post.author,
+					type: "like",
+					relatedUser: userId,
+					relatedPost: postId,
+				});
+
+				await newNotification.save();
+			}
+		}
+
+		await post.save();
+
+		res.status(200).json(post);
+	} catch (error) {
+		console.error("Error in likePost controller:", error);
+		res.status(500).json({ message: "Server error" });
+	}
+};
--- backend/routes/posts.route.js
+++ backend/routes/posts.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { protectedRoute, createPost, deletePost, getPostById } from "../middleware/auth.middleware.js";
+import { protectedRoute } from "../middleware/auth.middleware.js";
+import { likePost,createPost, deletePost, getPostById, createComment, getFeedPosts } from "../controllers/post.controller.js";
 
 const router= express.Router()
 
@@ -7,6 +8,10 @@ router.get('/',protectedRoute,getFeedPosts)
 router.post('/create',protectedRoute,createPost)
 router.delete('/delete/:id',protectedRoute,deletePost)
 router.get('/:id',protectedRoute,getPostById)
+
+router.get('/:id/comment',protectedRoute,createComment)
+router.get('/:id/like',protectedRoute,likePost)
+
 
 
 export default router
