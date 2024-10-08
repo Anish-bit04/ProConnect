93a5c6bc6bac58dfae494dfacfc9a0439b05e9aa
Anish <work.anish.004@gmail.com>
2024-10-09 01:41:20 +0530

    created api endpoint for connection handling

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import postsRoutes from "./routes/posts.route.js"
 import notificationsRoutes from "./routes/notifications.route.js"
+import connectionRoutes from "./routes/connection.route.js"
 
 import { connectDb } from "./lib/db.js"
 import cookieParser from "cookie-parser"
@@ -21,6 +22,7 @@ app.use('/api/v1/auth',authRoutes)
 app.use('/api/v1/user',userRoutes)
 app.use('/api/v1/posts',postsRoutes)
 app.use('/api/v1/notifications',notificationsRoutes)
+app.use('/api/v1/connection',connectionRoutes)
 
 
 app.listen(PORT, () => {
