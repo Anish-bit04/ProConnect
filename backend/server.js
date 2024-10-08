f40f5b1541dd606b018f54a6a32968a12b20169c
Anish <work.anish.004@gmail.com>
2024-10-09 05:23:41 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import  cors from "cors"
 
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
@@ -13,6 +14,10 @@ import cookieParser from "cookie-parser"
 dotenv.config()
 const app = express()
 
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use(express.json({limit:'50mb'}))
 app.use(cookieParser())
 
