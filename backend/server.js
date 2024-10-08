84ed400de5921a61c7fd6c44d6332ef2ac474bc8
Anish <work.anish.004@gmail.com>
2024-10-09 02:02:34 +0530

    added limit to json parser for image

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ import cookieParser from "cookie-parser"
 dotenv.config()
 const app = express()
 
-app.use(express.json())
+app.use(express.json({limit:'50mb'}))
 app.use(cookieParser())
 
 const PORT= process.env.PORT || 4000
