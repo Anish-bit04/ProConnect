472cd6af31785656ecdda588755f1b3dc21f91fe
Anish <work.anish.004@gmail.com>
2024-10-09 06:52:12 +0530

    create login and signup page -- not working error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,43 @@
 import Layout from "./components/Layout"
+import { Navigate, Routes,Route } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import SignUpPage from "./pages/auth/SignUpPage"
+import LoginPage from "./pages/auth/LoginPage"
+import toast, { Toaster } from "react-hot-toast"
+import { useQuery } from "@tanstack/react-query"
+import { axiosInstance } from "./lib/axios"
 
 function App() {
-  return <Layout>
-    hello
-  </Layout>
+
+  // const {data:authUser,isLoading} = useQuery({
+  //   queryKey:['authUser'],
+  //   queryFn:async()=>{
+  //     try{
+  //       const res = axiosInstance.get('/auth/me')
+  //       return res.data 
+  //     }
+  //     catch(err){
+  //       if(err.response && err.response.status == 401){
+  //         return null
+  //       }
+  //       toast.error(err.response.data.message || "Something went Wrong")
+  //     }
+  //   },
+    
+  // })
+
+  const authUser = false
+  // if(isLoading){
+  //   return null
+  // }
+  return (<Layout>
+    <Routes>
+        <Route path="/"  element={authUser?<HomePage/>:<Navigate to={'/login'}/>}/>
+        <Route path="/signup"  element={!authUser ? <SignUpPage/>:<Navigate to={'/'}/>}/>
+        <Route path="/login"  element={!authUser ? <LoginPage/> :<Navigate to={'/'} />}/>
+    </Routes>
+    <Toaster/>
+  </Layout>)
 }
 
 export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
